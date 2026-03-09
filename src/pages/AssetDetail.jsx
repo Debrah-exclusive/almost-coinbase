f67537fab704eb95bf98b24d9090e594fd8ddf3d
Debrah-exclusive <dk.debrah747@gmail.com>
2026-03-09 21:47:21 +0000

    ready

---
+++ src/pages/AssetDetail.jsx
@@ -0,0 +1,12 @@
+const AssetDetail = () => {
+  return (
+    <div className="w-full py-24 flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-title-1 text-gray-100 mb-2">Asset Detail</h1>
+        <p className="text-body text-gray-60">This page is under development.</p>
+      </div>
+    </div>
+  );
+};
+
+export default AssetDetail;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import HeroSection from '../components/sections/HeroSection';
+import ExploreCryptoSection from '../components/sections/ExploreCryptoSection';
+import AdvancedTraderSection from '../components/sections/AdvancedTraderSection';
+import CoinbaseOneSection from '../components/sections/CoinbaseOneSection';
+import BaseAppSection from '../components/sections/BaseAppSection';
+import LearnSection from '../components/sections/LearnSection';
+import TakeControlSection from '../components/sections/TakeControlSection';
+
+const Home = () => (
+  <div className="min-h-screen flex flex-col">
+    <Navbar />
+    <main className="flex-1">
+      <HeroSection />
+      <ExploreCryptoSection />
+      <AdvancedTraderSection />
+      <CoinbaseOneSection />
+      <BaseAppSection />
+      <LearnSection />
+      <TakeControlSection />
+    </main>
+    <Footer />
+  </div>
+);
+
+export default Home;
