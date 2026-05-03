b9e2de8b134847ba44d3a4f080d08a63771fb3a4
Debrah-exclusive <dk.debrah747@gmail.com>
2026-05-03 20:34:02 +0000

    Touches

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -17,8 +17,8 @@ const sendTokenResponse = (res, statusCode, user, message) => {
 
   const cookieOptions = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production', // HTTPS only in production
-    sameSite: 'lax',
+    secure: true, // Required for cross-site cookies
+    sameSite: 'none', // Required for cross-site cookies
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms
   };
 
